0d1eb10ad5bc927abe1ed915309c1d396a992878
Lauren <me@lauren-lizardo.com>
2020-02-01 19:10:22 -0800

    Add input to main chicken; dialogue system setup for npc chickens

--- Assets/Scripts/MainChickenController.cs
+++ Assets/Scripts/MainChickenController.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class MainChickenController : MonoBehaviour
 {
-  public float InteractionRadius = 2f;
+  public float InteractionProximity = 2f;
   public GameObject LoveChicken;
   public GameObject NeighborChicken;
 
+  private bool _inProximity;
+  private bool _LoveChickenVisible;
+
   private void Update()
   {
-    if ( (Vector3.Distance(transform.position, LoveChicken.transform.position) <= InteractionRadius) && LoveChicken.GetComponent<Renderer>().isVisible)
+    _inProximity = (Vector3.Distance(transform.position, LoveChicken.transform.position) <= InteractionProximity) ? true : false;
+    _LoveChickenVisible = LoveChicken.GetComponent<Renderer>().isVisible;
+
+    if ( _inProximity && _LoveChickenVisible)
     {
       // Show dialogue
       Debug.Log("LoveChicken is in view");
+
+      if (OVRInput.GetUp(OVRInput.Button.One) || OVRInput.GetUp(OVRInput.Button.Three))
+      {
+        LoveChicken.GetComponent<NPCChicken>().TextArea.text += LoveChicken.GetComponent<NPCChicken>().Name;
+      }
     }
+
+
   }
 }
---
+++ Assets/Scripts/NPCChicken.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+  // * DialogueGroups (each group corresponds to the current objective);
+  //   * DialogueGroup
+  //     * dialogue
+  //     * dialogue
+  //     * dialogue
+  //     * dialogue
+  //     * dialogue
+  //     * dialogue
+  //     * dialogue
+  //   * DialogueGroup
+  //     * dialogue
+  //     * dialogue
+  //     * dialogue
+  //   * DialogueGroup
+  //     * dialogue
+  //     * dialogue
+  //     * dialogue
+  //   * DialogueGroup
+  //     * dialogue
+  //     * dialogue
+  //     * dialogue
+
+public class NPCChicken : MonoBehaviour
+{
+  public string Name;
+  public Text TextArea;
+  
+  [System.Serializable]
+  public class Dialogue
+  {
+    public string DialogueText;
+    public int NextDialogueIndex;
+  }
+
+  [System.Serializable]
+  public class DialogueGroup
+  {
+    public List<Dialogue> Dialogues = new List<Dialogue>();
+  }
+
+  public List<DialogueGroup> DialogueGroups = new List<DialogueGroup>();
+
+
+
+}
