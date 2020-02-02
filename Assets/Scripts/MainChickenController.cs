968922f775bec8bc00ab83c256b13a93c3ba845a
Lauren <me@lauren-lizardo.com>
2020-02-01 18:39:22 -0800

    Added new main chicken controller scripts; add mesh renderers to chickens

---
+++ Assets/Scripts/MainChickenController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainChickenController : MonoBehaviour
+{
+  public float InteractionRadius = 2f;
+  public GameObject LoveChicken;
+  public GameObject NeighborChicken;
+
+  private void Update()
+  {
+    if ( (Vector3.Distance(transform.position, LoveChicken.transform.position) <= InteractionRadius) && LoveChicken.GetComponent<Renderer>().isVisible)
+    {
+      // Show dialogue
+      Debug.Log("LoveChicken is in view");
+    }
+  }
+}
