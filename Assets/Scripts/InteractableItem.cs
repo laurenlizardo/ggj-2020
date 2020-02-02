bc5d2879d2404758952ef15fb36d84ee1fae67b8
Lauren <me@lauren-lizardo.com>
2020-02-01 23:21:02 -0800

    New script InteractableItem

---
+++ Assets/Scripts/InteractableItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach this to all OVRGrabbable props
+
+public class InteractableItem : MonoBehaviour
+{
+  public string ItemID;
+}
