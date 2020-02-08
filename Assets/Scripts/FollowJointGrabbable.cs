922c9ce22aab59e5d8d2e6c1584d01efb8f6cad9
Lauren <me@lauren-lizardo.com>
2020-02-07 16:55:29 -0800

    Added hinged/fixed joint mechanic

---
+++ Assets/Scripts/FollowJointGrabbable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script allows a game object with a fixed joint component to follow the child object that contains the grabbable component
+
+public class FollowJointGrabbable : MonoBehaviour
+{
+  public Transform GrabbableTransform;
+  public Rigidbody Rigidbody;
+
+  private void FixedUpdate()
+  {
+    Rigidbody.MovePosition(GrabbableTransform.transform.position);
+  }
+}
---
+++ Assets/Scripts/JointGrabbable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointGrabbable : OVRGrabbable
+{
+  public Transform FixedJointTransform;
+  public float ReleaseThreshold;
+
+  public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+  {
+    base.GrabEnd(Vector3.zero, Vector3.zero);
+    transform.position = FixedJointTransform.transform.position;
+    transform.rotation = FixedJointTransform.transform.rotation;
+
+    Rigidbody fixedJointRb = FixedJointTransform.GetComponent<Rigidbody>();
+    fixedJointRb.velocity = Vector3.zero;
+    fixedJointRb.angularVelocity = Vector3.zero;
+  }
+
+  private void Update()
+  {
+    if (Vector3.Distance (FixedJointTransform.position, this.transform.position) > ReleaseThreshold)
+    {
+      grabbedBy.ForceRelease(this);
+    }
+  }
+}
