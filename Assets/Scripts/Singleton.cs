c722d787d7715e7061fba2c2c7366cd2fa8a8280
Lauren <me@lauren-lizardo.com>
2020-03-24 05:53:30 -0700

    Added generic abstract singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A generic implementation of the singleton design pattern for Unity development.
+/// </summary>
+public abstract class Singleton<T> : MonoBehaviour where T : Component
+{
+  private static T _instance;
+  public static T Instance
+  {
+    get
+    {
+      if (_instance == null)
+      {
+        _instance = FindObjectOfType<T>();
+        if (_instance == null) _instance = new GameObject().AddComponent<T>();
+      }
+
+      _instance.name = string.Format("{0} (Singleton)", typeof(T).ToString());
+      DontDestroyOnLoad(_instance);
+
+      return _instance;
+    }
+  }
+}
