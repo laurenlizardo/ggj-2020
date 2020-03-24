25dea3acc4aceade79d1cb97b1c3752280d05161
Lauren <me@lauren-lizardo.com>
2020-03-24 06:31:43 -0700

    Inherited from generic singleton; renamed variables

--- Assets/Scripts/MainChickenController.cs
+++ Assets/Scripts/MainChickenController.cs
@@ -3,50 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MainChickenController : MonoBehaviour
+public class MainChickenController : Singleton<MainChickenController>
 {
-  private static MainChickenController _instance;
-  public static MainChickenController Instance => _instance;
-
-  private void Awake()
-  {
-    _instance = this;
-  }
-
   public Text DebugText;
   
   public float InteractionProximity = 2f;
-  public GameObject LoveChicken;
+  public GameObject GrumpyChicken;
   public GameObject NeighborChicken;
 
-  private bool _inLoveChickenProximity;
-  private bool _LoveChickenVisible;
+  private bool _inGrumpyChickenProximity;
+  private bool _isGrumpyChickenVisible;
 
   private bool _inNeighborChickenProximity;
-  private bool _NeighborChickenVisible;
+  private bool _isNeighborChickenVisible;
 
-  public GameObject LeftGrabber;
-  public GameObject RightGrabber;
+  public GameObject LeftHandGrabber;
+  public GameObject RightHandGrabber;
 
-  public bool HoldingItem;
+  public bool IsHoldingItem;
   public List<GameObject> HeldItems = new List<GameObject>();
 
   private void Update()
   {
-    _inLoveChickenProximity = (Vector3.Distance(transform.position, LoveChicken.transform.position) <= InteractionProximity) ? true : false;
-    _LoveChickenVisible = LoveChicken.GetComponent<Renderer>().isVisible;
+    _inGrumpyChickenProximity = (Vector3.Distance(transform.position, GrumpyChicken.transform.position) <= InteractionProximity) ? true : false;
+    _isGrumpyChickenVisible = GrumpyChicken.GetComponent<Renderer>().isVisible;
 
-    _NeighborChickenVisible = (Vector3.Distance(transform.position, NeighborChicken.transform.position) <= InteractionProximity) ? true : false;
-    _NeighborChickenVisible = NeighborChicken.GetComponent<Renderer>().isVisible;
+    _isNeighborChickenVisible = (Vector3.Distance(transform.position, NeighborChicken.transform.position) <= InteractionProximity) ? true : false;
+    _isNeighborChickenVisible = NeighborChicken.GetComponent<Renderer>().isVisible;
 
 // DISTANCE CHECKING
-    if (Vector3.Distance(transform.position, LoveChicken.transform.position) <= InteractionProximity && !LoveChicken.GetComponent<NPCChicken>().IsSpeaking)// && LoveChicken.GetComponent<Renderer>().isVisible)
+    if (Vector3.Distance(transform.position, GrumpyChicken.transform.position) <= InteractionProximity && !GrumpyChicken.GetComponent<NPCChicken>().IsSpeaking)// && LoveChicken.GetComponent<Renderer>().isVisible)
     {
-      LoveChicken.GetComponent<NPCChicken>().TalkButton.SetActive(true);
+      GrumpyChicken.GetComponent<NPCChicken>().TalkButton.SetActive(true);
     }
     else
     {
-      LoveChicken.GetComponent<NPCChicken>().TalkButton.SetActive(false);
+      GrumpyChicken.GetComponent<NPCChicken>().TalkButton.SetActive(false);
     }
 
     if (Vector3.Distance(transform.position, NeighborChicken.transform.position) <= InteractionProximity && !NeighborChicken.GetComponent<NPCChicken>().IsSpeaking)// && NeighborChicken.GetComponent<Renderer>().isVisible)
@@ -59,19 +51,19 @@ public class MainChickenController : MonoBehaviour
     }
 
 // ITEM CHECKING
-    if(LeftGrabber.GetComponent<OVRGrabber>().GrabbedObject == null && RightGrabber.GetComponent<OVRGrabber>().GrabbedObject == null) HoldingItem = false;
-    else if (LeftGrabber.GetComponent<OVRGrabber>().GrabbedObject != null || RightGrabber.GetComponent<OVRGrabber>().GrabbedObject != null) HoldingItem = true;
+    if(LeftHandGrabber.GetComponent<OVRGrabber>().GrabbedObject == null && RightHandGrabber.GetComponent<OVRGrabber>().GrabbedObject == null) IsHoldingItem = false;
+    else if (LeftHandGrabber.GetComponent<OVRGrabber>().GrabbedObject != null || RightHandGrabber.GetComponent<OVRGrabber>().GrabbedObject != null) IsHoldingItem = true;
 
-    if (HoldingItem)
+    if (IsHoldingItem)
     {
-      if (LeftGrabber.GetComponent<OVRGrabber>().GrabbedObject != null) HeldItems.Add(LeftGrabber.GetComponent<OVRGrabber>().GrabbedObject.gameObject);
-      if (RightGrabber.GetComponent<OVRGrabber>().GrabbedObject != null) HeldItems.Add(RightGrabber.GetComponent<OVRGrabber>().GrabbedObject.gameObject);
+      if (LeftHandGrabber.GetComponent<OVRGrabber>().GrabbedObject != null) HeldItems.Add(LeftHandGrabber.GetComponent<OVRGrabber>().GrabbedObject.gameObject);
+      if (RightHandGrabber.GetComponent<OVRGrabber>().GrabbedObject != null) HeldItems.Add(RightHandGrabber.GetComponent<OVRGrabber>().GrabbedObject.gameObject);
     }
     else
     {
       HeldItems.Clear();
     }
 
-    DebugText.text = string.Format("Holding Item? {0} \nHeld Items: {1}, {2}", HoldingItem, HeldItems[0], HeldItems[1]);
+    DebugText.text = string.Format("Holding Item? {0} \nHeld Items: {1}, {2}", IsHoldingItem, HeldItems[0], HeldItems[1]);
   }
 }
