32021d532278679faf9d25c19d91b406e3f84568
Lauren <me@lauren-lizardo.com>
2020-03-25 06:03:13 -0700

    Added Messenger script

---
+++ Assets/Scripts/Messenger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+public delegate void Callback();
+
+public static class Messenger
+{
+    private static Dictionary<string, Delegate> eventTable = new Dictionary<string, Delegate>();
+
+    public static void AddListener(string eventType, Callback handler)
+    {
+        lock (eventTable)
+        {
+            if (!eventTable.ContainsKey(eventType))
+            {
+                eventTable.Add(eventType, null);
+                Debug.Log(string.Format("ADDED: Listener ({0}) to Dictionary({1}).", eventType, eventTable.ToString()));
+            }
+            eventTable[eventType] = (Callback)eventTable[eventType] + handler;
+        }
+    }
+
+    public static void RemoveListener(string eventType, Callback handler)
+    {
+        lock (eventTable)
+        {
+            if (eventTable.ContainsKey(eventType))
+            {
+                eventTable[eventType] = (Callback)eventTable[eventType] - handler;
+
+                if (eventTable[eventType] == null)
+                {
+                    eventTable.Remove(eventType);
+                }
+            }
+        }
+    }
+
+    public static void Invoke(string eventType)
+    {
+        if (eventTable.TryGetValue(eventType, out Delegate d))
+        {
+            ((Callback)d)?.Invoke();
+        }
+    }
+}
